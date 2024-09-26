ee6ce94039de319f34ca85e01307dd2f3b1ae4c2
Salmathsalam-V <147300874+Salmathsalam-V@users.noreply.github.com>
2024-09-26 07:06:18 +0530

    Update custom_filters.py

--- Store/templatetags/custom_filters.py
+++ Store/templatetags/custom_filters.py
@@ -1,18 +1,1 @@
-from django import template
 
-register = template.Library()
-
-@register.filter
-def range_filter(value):
-    return range(1, value + 1)
-
-@register.filter
-def get_item(dictionary, key):
-    return dictionary.get(key)
-
-@register.filter(name='multiply')
-def multiply(value, arg):
-    try:
-        return value * arg
-    except (ValueError, TypeError):
-        return ''
