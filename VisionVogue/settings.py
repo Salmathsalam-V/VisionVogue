7ad6c2fdc6add0734785f0ed63a27cefebc21f50
Salmathsalam-V <147300874+Salmathsalam-V@users.noreply.github.com>
2024-09-26 07:34:24 +0530

    Update settings.py

--- VisionVogue/settings.py
+++ VisionVogue/settings.py
@@ -182,7 +182,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Do not include STATIC_ROOT here
 ]
