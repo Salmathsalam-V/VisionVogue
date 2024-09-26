18cf8a6d951d977689f314fb30401bae678866f4
Salmathsalam-V <147300874+Salmathsalam-V@users.noreply.github.com>
2024-09-26 07:07:07 +0530

    Update custom_filters.py

--- Admin/templatetags/custom_filters.py
+++ Admin/templatetags/custom_filters.py
@@ -5,3 +5,19 @@ register = template.Library()
 @register.filter(name='add_class')
 def add_class(value, css_class):
     return value.as_widget(attrs={"class": css_class})
+
+
+@register.filter
+def range_filter(value):
+    return range(1, value + 1)
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
+@register.filter(name='multiply')
+def multiply(value, arg):
+    try:
+        return value * arg
+    except (ValueError, TypeError):
+        return ''
