b78b4055c8b474777ec098c84377c491cc38d38c
Salmathsalam-V <147300874+Salmathsalam-V@users.noreply.github.com>
2024-09-26 20:05:31 +0530

    Update forms.py

--- Orders/forms.py
+++ Orders/forms.py
@@ -8,15 +8,3 @@ class AddressForm(forms.ModelForm):
         fields = ['first_name', 'last_name', 'phone', 'email', 'address_line_1', 'address_line_2', 'country', 'state', 'city']
 
 
-
-# PAYMENT_METHOD_CHOICES = [
-#     ('COD', 'Cash on Delivery'),
-#     ('Razorpay', 'Razorpay'),
-# ]
-
-# class PaymentForm(forms.ModelForm):
-#     payment_method = forms.ChoiceField(choices=PAYMENT_METHOD_CHOICES, widget=forms.RadioSelect)
-
-#     class Meta:
-#         model = Payment
-#         fields = ['payment_method']
